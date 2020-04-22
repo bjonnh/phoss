8623b3ef35186e117c8c5f9ae24051716f3711a6
Jonathan Bisson <research@bjonnh.net>
2020-04-22 09:47:17 -0500

    Add Velocity templating engine.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     kotlin("jvm") version "1.4-M1"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "net.nprod"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -17,18 +18,20 @@ dependencies {
     implementation("org.slf4j", "slf4j-log4j12", "1.7.29")
     implementation("org.openscience.cdk", "cdk-bundle", "2.3")
     implementation("org.apache.commons", "commons-compress", "1.20")
+    implementation("org.apache.velocity", "velocity-engine-core", "2.2")
+    implementation("org.apache.velocity.tools", "velocity-tools-generic", "3.0")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_13
 }
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "13"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "13"
     }
 }
 
